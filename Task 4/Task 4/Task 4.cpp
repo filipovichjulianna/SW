b7a5a99b25b48316cf0679fe8b84b5bd77eef09a
filipovichjulianna <91153106+filipovichjulianna@users.noreply.github.com>
2021-09-29 22:44:00 +0300

    add Task 4

---
+++ Task 4/Task 4/Task 4.cpp
@@ -0,0 +1,29 @@
+﻿#include <iostream>
+#include <math.h>
+using namespace std;
+
+int main()
+
+{
+	setlocale(LC_ALL, "RUS");
+	int l, w, h, l1, w1;
+	cout << "Введите длину, ширину, высоту параллелипипеда: " << endl; 
+	cin >> l;
+    cin >>w;	
+	cin >> h;
+	cout << "Ввндите длину и ширину отверстия : " << endl;
+	cin >> l1;
+	cin >> w1;
+	if ((l >= l1) && (w >= w1)) {
+
+		cout << "Параллелипипед проходит в отверстие";
+
+	}
+
+	else {
+
+		cout << "Параллелипипед проходит в отверстие";
+
+	}
+
+}
