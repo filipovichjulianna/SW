94d0b52c0069af8d60295c61148a2ce3222100a9
filipovichjulianna <91153106+filipovichjulianna@users.noreply.github.com>
2021-09-29 22:43:06 +0300

    add Task 1

---
+++ Task 1.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+#include <math.h>
+using namespace std;
+
+int main()
+
+{
+	setlocale(LC_ALL, "RUS");
+	int x;
+	cout << "¬ведите x : "; cin >> x;
+	int raz = 0;
+	int ten = 1;
+	while (true) {
+        ten *= 10;
+		if (ten > x) {
+			ten /= 10;
+			while (ten >= 1) {
+				raz = x / ten;
+				cout << raz << endl;
+				x %= ten;
+				ten /= 10;
+
+			}
+
+
+
+		}
+
+	}
+
+}
