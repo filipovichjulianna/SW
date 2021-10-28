d65f99dad1c138363b857f5061c4ce6c08eef8f0
filipovichjulianna <91153106+filipovichjulianna@users.noreply.github.com>
2021-10-28 12:18:51 +0300

    Task 1(2)

---
+++ Task 1(2)/Task 1(2)/Task 1(2).cpp
@@ -0,0 +1,29 @@
+﻿#include <iostream>
+using namespace std;
+
+int main()
+{
+	setlocale(LC_ALL, "RUS");
+	int length;
+	cout << "Введите длину массива:"; cin >> length;
+	srand(time(0));
+	int* arr = new int[length];
+	for (int i = 0; i < length; i++)
+	{
+		arr[i] = rand() % 100 - 50;
+		cout << "a[" << i << "]=" << arr[i] << endl;
+	}
+	int numb;
+	cout << "Введите число: ";
+	cin >> numb;
+	for (int i = 0; i < length; i++)
+	{
+		if (arr[i] == numb) {
+			cout << "Позиция числа " << numb << "-" << i;
+			return 0;
+		}
+	}
+
+
+
+}
